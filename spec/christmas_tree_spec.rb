a472471ca9770d7aa8e061561b81ec3cc9900c0e
simon <simon-isler@gmx.ch>
2020-08-13 13:01:35 +0200

    Add TODOs

--- spec/christmas_tree_spec.rb
+++ spec/christmas_tree_spec.rb
@@ -8,10 +8,12 @@ RSpec.describe ChristmasTree do
   let(:height) { 5 }
   let(:tree_characters_count) { christmas_tree.count(described_class::TREE_CHARACTER) }
 
+  # TODO: find a way to improve this test. Currently it just counts the occurences of TREE_CHARACTER
   it 'draws the christmas tree correctly' do
     expect(tree_characters_count).to eq(height ** 2)
   end
 
   context 'with star and tree stand' do
+    # TODO: write a test for the advanced task
   end
 end
